30062c386af4321c096e1d2c1a7b74321dfabc0c
ravthan <ravthan@2862ca4a-4f57-e750-6f19-d5026df31d27>
2012-10-10 04:21:27 +0000

    Initial All Eyes Framework.  Note we have two Makefiles.  One for Ubuntu and one for RedHat.

---
+++ src/ae/selfmon.c
@@ -0,0 +1,66 @@
+/*
+ * Copyright (C) <2012> <Blair Wolfinger, Ravi Jagannathan, Thomas Pari, Todd Chu>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this
+ * software and associated documentation files (the "Software"), to deal in the Software
+ * without restriction, including without limitation the rights to use, copy, modify,
+ * merge, publish, distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+ * The above copyright notice and this permission notice shall be included in all copies
+ * or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+ * PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR
+ * ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ * ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *
+ * Original Author: Ravi Jagannathan
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <time.h>
+#include <ctype.h>
+#include <string.h>
+#include <syslog.h>
+#include <errno.h>
+#include <pwd.h>
+#include <signal.h>
+#include <dirent.h>
+#include <pthread.h>
+#include <sys/socket.h>
+#include <sys/un.h>
+#include <sys/time.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/resource.h>
+#include <sys/file.h>
+#include <sys/select.h>
+#include <sys/stat.h>
+#include "ae.h"
+
+// Only exposed to this thread.
+extern MONCOMM monarray[];
+
+void *
+selfMon(void *myStruct)
+{
+MONCOMM *myMon;
+
+    myMon = (MONCOMM *)myStruct;
+
+    aeLOG("selfMon is Executing\n");
+    aeLOG("selfMon is Executing\n");
+    aeLOG("selfMon name: %s\n", myMon->name);
+    aeLOG("selfMon mode: 0x%x\n", myMon->mode);
+    aeLOG("selfMon lifespan: 0x%x\n", myMon->span);
+
+    while (1)  {
+    }
+}
+
