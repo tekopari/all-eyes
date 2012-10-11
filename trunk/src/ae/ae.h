7b9ba23a4df2a72f4489e1893fdb795a956102e9
ravthan <ravthan@2862ca4a-4f57-e750-6f19-d5026df31d27>
2012-10-11 05:38:19 +0000

    Added Socket IPC

--- trunk/src/ae/ae.h
+++ trunk/src/ae/ae.h
@@ -78,8 +78,9 @@ void aeSigHdlr(int sig, siginfo_t *siginfo, void *context);
 /*
  * Error codes in ae
  */
-#define SPAWN_MONITOR_ERROR 1
-#define SIGACTION_ERROR     2
+#define SPAWN_MONITOR_ERROR    1
+#define SIGACTION_ERROR        2
+#define RESOURCE_UNAVAIL_EXIT  3
 
 
 /*
@@ -90,16 +91,24 @@ typedef struct monComm  {
     unsigned int        mode;      // Volatile or persistent
     unsigned int        span;      // lives across reboot or not.
     unsigned int        status;    // status is good or bad. Monitor fills.
-    unsigned int        ppid;      // Mother monitor's PID
+    unsigned int        pid;       // Monitor's PID
+    unsigned int        ppid;      // ae daemon's PID
     unsigned int        action;    // Filled by the Monitor
     unsigned int        heartbeat; // Filled by the Monitor
-    char                *basedir;  // Base directory under which Monitors will store their persistent data
+    char                *basedir;  // Dir for Monitors to store persistent data
     pthread_mutex_t     monMutex;  // Monitor Mutex, used by the monitor
-    void (*monPtr)(void);         // Entry point of the Monitor. Look in ae.c
+    int                 socFd[2];  // socket IPC between ae daemon & monitor
+                                   //   ae will use 0th socket; monitor 1st.
+    void (*monPtr)(void);          // Entry point of the Monitor. Look in ae.c
 } MONCOMM;
+
+#define MAXMONITORS    4
+
 
 extern void monLock(pthread_mutex_t *mutexPtr);
 extern void monUnlock(pthread_mutex_t *mutexPtr);
+extern int getSocPair(int *ptr);
+extern void gracefulExit(void);
 
 extern void selfMon(void);
 extern void socketMon(void);
