c4b2269a0f32bad1e095e0134132848970d2eafa
todddchu <todddchu@2862ca4a-4f57-e750-6f19-d5026df31d27>
2012-10-20 20:08:43 +0000

    Add get_pkg.sh for installing packages under chroot

---
+++ src/ae/install/get_pkg.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+########################################################################################################
+# copyright (C) <2012> <Blair Wolfinger, Ravi Jagannathan, Thomas Pari, Todd Chu>
+# 
+# Permission is hereby granted, free of charge, to any person obtaining a copy of this 
+# software and associated documentation files (the "Software"), to deal in the Software 
+# without restriction, including without limitation the rights to use, copy, modify, 
+# merge, publish, distribute, sublicense, and/or sell copies of the Software, and to 
+# permit persons to whom the Software is furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all copies 
+# or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, 
+# INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR 
+# PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR 
+# ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, 
+# ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+#
+# Original Author: Todd Chu
+########################################################################################################
+#
+# File Name: get_pkg.sh
+# Description:
+#    This script is called by its parent script 'install_chroot.sh"
+#    to install the packages for running All-Eyes
+#
+########################################################################################################
+clear
+echo "*** Install packages ..."
+apt-get install vim
+apt-get install net-tools
+apt-get install openssl
+apt-get install apparmor
+apt-get install apparmor-profiles
+echo "*** Done!"
+echo ""
+echo "*********************************************************"
+echo "***  TO exit the chroot, please issue command 'exit'  ***"
+echo "*********************************************************"
