42612674fb8d12230468d6d353a16d3d08a3a8af
todddchu <todddchu@2862ca4a-4f57-e750-6f19-d5026df31d27>
2012-10-21 22:18:37 +0000

    typo

--- src/ae/install/install_chroot.sh
+++ src/ae/install/install_chroot.sh
@@ -85,7 +85,7 @@ des_dir=$jail_dir/bin
 sudo mkdir -p $des_dir
 for file in $src_dir/*
 do
-   if [ "$file" != "$0" ] && [ "$file" != "$src_dir/read_me_first"] 
+   if [ "$file" != "$0" ] && [ "$file" != "$src_dir/read_me_first" ] 
    then
       sudo cp $file $des_dir/.
       perm=$(sudo stat -c "%a" $file)
