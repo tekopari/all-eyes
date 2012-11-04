29570021cf3777cacd1b67fbeab25474118b5ae7
tbrt.tom@gmail.com <tbrt.tom@gmail.com@2862ca4a-4f57-e750-6f19-d5026df31d27>
2012-11-04 22:27:05 +0000

    Fixed the classs name in the script

--- src/proxy/AeProxy/src/execute.sh
+++ src/proxy/AeProxy/src/execute.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-export CLASSPATH=/usr/lib/lightdm/lightdm:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:.
-java aeProxy 127.0.0.1 6000
+export CLASSPATH=.:$CLASSPATH
+java AeProxy 127.0.0.1 6000
 
