60cea9da8e47309a0b6ea32e07f0a9af3e1c8c2b
tbrt.tom@gmail.com <tbrt.tom@gmail.com@2862ca4a-4f57-e750-6f19-d5026df31d27>
2012-11-06 19:10:48 +0000

    Added the options in the makefile to build the package

---
+++ trunk/src/proxy/AeProxy/init.d/install_aeproxy.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+#
+# INSTALL THE AEPROXY SERVICE
+#
+
+sudo /bin/chown root:root /etc/ae/certs/jssecacerts
+sudo /bin/chmod 744 /etc/ae/certs/jssecacerts
+
+sudo /bin/chown root:root /etc/ae/certs/keystore.jks
+sudo /bin/chmod 744 /etc/ae/certs/keystore.jks
+
+sudo /bin/chown root:root /usr/local/bin/aeproxy
+sudo /bin/chmod 6755 /usr/local/bin/aeproxy
+
+sudo /bin/chown root:root /usr/local/bin/AeProxy.jar
+sudo /bin/chmod 755 /usr/local/bin/AeProxy.jar
+
+sudo /bin/chown root:root /etc/init.d/aeproxyd
+sudo /bin/chmod 755 /etc/init.d/aeproxyd
+
+sudo /bin/ln -s /etc/init.d/aeproxyd /etc/rc0.d/K99aeproxyd
+sudo /bin/ln -s /etc/init.d/aeproxyd /etc/rc1.d/K99aeproxyd
+sudo /bin/ln -s /etc/init.d/aeproxyd /etc/rc2.d/S99aeproxyd
+sudo /bin/ln -s /etc/init.d/aeproxyd /etc/rc3.d/S99aeproxyd
+sudo /bin/ln -s /etc/init.d/aeproxyd /etc/rc4.d/S99aeproxyd
+sudo /bin/ln -s /etc/init.d/aeproxyd /etc/rc5.d/S99aeproxyd
+sudo /bin/ln -s /etc/init.d/aeproxyd /etc/rc6.d/K99aeproxyd
+
