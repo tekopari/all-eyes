b4eb8bedb35c33a5fbc118d417ff1c69b27fd56d
tbrt.tom@gmail.com <tbrt.tom@gmail.com@2862ca4a-4f57-e750-6f19-d5026df31d27>
2012-11-06 21:40:29 +0000

    Check in the initial android application.  It is incomplete due to Hurricane Sandy.

---
+++ trunk/src/aemgr/aeManager/src/org/tbrt/aemanager/AeManagerSettings.java
@@ -0,0 +1,35 @@
+package org.tbrt.aemanager;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.support.v4.app.NavUtils;
+
+public class AeManagerSettings extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ae_manager_settings);
+        getActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_ae_manager_settings, menu);
+        return true;
+    }
+
+    
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                NavUtils.navigateUpFromSameTask(this);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+}
