f03d0857bf24fe5dc46078a98c119efc101626ae
todddchu <todddchu@2862ca4a-4f57-e750-6f19-d5026df31d27>
2012-11-13 04:38:02 +0000

    Make file for ae daemon and AeProxy

---
+++ trunk/src/install_all-eyes.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+########################################################################################################
+# copyright (C) <2012> <Blair Wolfinger, Ravi Jagannathan, Thomas Pari, Todd Chu>
+# 
+# Permission is hereby granted, free of charge, to any person obtaining a copy of this 
+# software and associated documentation files (the "Software"), to deal in the Software 
+# without restriction, including without limitation the rights to use, copy, modify, 
+# merge, publish, distribute, sublicense, and/or sell copies of the Software, and to 
+# permit persons to whom the Software is furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all copies 
+# or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, 
+# INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR 
+# PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR 
+# ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, 
+# ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+#
+# Original Author: Todd Chu
+########################################################################################################
+#
+# File Name: install_alleyes.sh
+# Description:
+#    Run this script to install the tar file all-eyes.tgz. This includes the following steps
+#        (1) untar the file all-eyes.tgz
+#        (2) untar the sub-tar files
+#        (3) travels each directory and install files
+########################################################################################################
+clear
+mydir=`pwd`
+
+echo  "****** Untab all-eyes.tgz ..."
+tar xvfz all-eyes.tgz
+
+echo "****** Install AeProxy ..."
+cd /
+sudo tar xvf ${mydir}/aeproxy.tar
+sudo /etc/init.d/install_aeproxy.sh
+sudo rm -f /etc/init.d/install_aeproxy.sh
+sudo service aeproxyd start
+
+echo "****** Install ae daemon ..."
+cd  ${mydir}
+tar xvfz ae-daemon.tgz
+cd  ${mydir}/product
+./install_chroot.sh
+exit 0
