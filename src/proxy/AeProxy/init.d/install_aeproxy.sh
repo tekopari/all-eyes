61510a34d5b4631c8058ad6190e0e7110b7c944b
todddchu <todddchu@2862ca4a-4f57-e750-6f19-d5026df31d27>
2012-11-13 04:14:20 +0000

    add -f when creating soft-links

--- src/proxy/AeProxy/init.d/install_aeproxy.sh
+++ src/proxy/AeProxy/init.d/install_aeproxy.sh
@@ -19,11 +19,11 @@ sudo /bin/chmod 755 /usr/local/bin/AeProxy.jar
 sudo /bin/chown root:root /etc/init.d/aeproxyd
 sudo /bin/chmod 755 /etc/init.d/aeproxyd
 
-sudo /bin/ln -s /etc/init.d/aeproxyd /etc/rc0.d/K99aeproxyd
-sudo /bin/ln -s /etc/init.d/aeproxyd /etc/rc1.d/K99aeproxyd
-sudo /bin/ln -s /etc/init.d/aeproxyd /etc/rc2.d/S99aeproxyd
-sudo /bin/ln -s /etc/init.d/aeproxyd /etc/rc3.d/S99aeproxyd
-sudo /bin/ln -s /etc/init.d/aeproxyd /etc/rc4.d/S99aeproxyd
-sudo /bin/ln -s /etc/init.d/aeproxyd /etc/rc5.d/S99aeproxyd
-sudo /bin/ln -s /etc/init.d/aeproxyd /etc/rc6.d/K99aeproxyd
+sudo /bin/ln -s -f /etc/init.d/aeproxyd /etc/rc0.d/K99aeproxyd
+sudo /bin/ln -s -f /etc/init.d/aeproxyd /etc/rc1.d/K99aeproxyd
+sudo /bin/ln -s -f /etc/init.d/aeproxyd /etc/rc2.d/S99aeproxyd
+sudo /bin/ln -s -f /etc/init.d/aeproxyd /etc/rc3.d/S99aeproxyd
+sudo /bin/ln -s -f /etc/init.d/aeproxyd /etc/rc4.d/S99aeproxyd
+sudo /bin/ln -s -f /etc/init.d/aeproxyd /etc/rc5.d/S99aeproxyd
+sudo /bin/ln -s -f /etc/init.d/aeproxyd /etc/rc6.d/K99aeproxyd
 
