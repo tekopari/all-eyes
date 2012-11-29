40521c8982c16dd3cc09e154937dc9de943abe5a
heyyoulive <heyyoulive@2862ca4a-4f57-e750-6f19-d5026df31d27>
2012-11-29 03:54:52 +0000

    Add check for 0-9

--- trunk/src/ae/filemon/filemonConfig.c
+++ trunk/src/ae/filemon/filemonConfig.c
@@ -83,11 +83,11 @@ int create_checksum_filemon(char *file_name, FILE *chksumFH)
 int check_if_alpha(char buf[1])
 {
 
-
 	if( !isalpha(buf[0]) )
 	{
 		if((strncmp(&buf[0], "/", 1) != 0) && (strncmp(&buf[0], "_", 1) != 0)
-			&& (strncmp(&buf[0], "-", 1) !=0) && (strncmp(&buf[0], ".", 1) != 0))
+			&& (strncmp(&buf[0], "-", 1) !=0) && (strncmp(&buf[0], ".", 1) != 0)
+			&& !isdigit(buf[0]))
 		{
 			return (-1);
 		}
