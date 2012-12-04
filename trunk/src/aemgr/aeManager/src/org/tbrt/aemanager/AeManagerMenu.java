6e264659ea314f920b48525865c9586ffb548dba
tbrt.tom@gmail.com <tbrt.tom@gmail.com@2862ca4a-4f57-e750-6f19-d5026df31d27>
2012-12-04 05:26:03 +0000

    Added the code that performs authentication and authorization.

--- trunk/src/aemgr/aeManager/src/org/tbrt/aemanager/AeManagerMenu.java
+++ trunk/src/aemgr/aeManager/src/org/tbrt/aemanager/AeManagerMenu.java
@@ -21,9 +21,20 @@
 
 package org.tbrt.aemanager;
 
+import java.io.IOException;
+
+import org.tbrt.aemanager.AeManagerEventList.SendActionAsyncTask;
+
+import com.google.android.gms.auth.*;
+import com.google.android.gms.common.*;
+
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.accounts.AccountManager;
 import android.app.Activity;
+import android.app.Dialog;
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.SharedPreferences.OnSharedPreferenceChangeListener;
@@ -31,11 +42,16 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
 import android.support.v4.app.NavUtils;
 
 public class AeManagerMenu extends Activity implements OnSharedPreferenceChangeListener {
 	
     private static final String TAG = "AeManagerMenu";
+	static final int AE_ACCT_REQUEST = 1;  
+	static final int REQUEST_TOKEN = 2;
+    public static String token = null;
+    public static String accountName = null;
     
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -43,7 +59,97 @@ public class AeManagerMenu extends Activity implements OnSharedPreferenceChangeL
         setContentView(R.layout.activity_ae_manager_menu);
         getActionBar().setDisplayHomeAsUpEnabled(true);
         loadPreferences();
+        int rc = GooglePlayServicesUtil.isGooglePlayServicesAvailable(this.getApplicationContext());
+		if( rc == ConnectionResult.SUCCESS ) { 
+            Intent intent = AccountPicker.newChooseAccountIntent(null, null, new String[]{"com.google"}, false, null, null, null, null);
+            startActivityForResult(intent, AE_ACCT_REQUEST);
+        }
+        else {
+        	Toast.makeText(getApplicationContext(), "GOOGLE PLAY SERVICES NOT AVAILABLE", Toast.LENGTH_SHORT).show();
+            SharedPreferences settings = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+        	token = settings.getString("password", "");
+            accountName = settings.getString("username", "");
+        	GooglePlayServicesUtil.getErrorDialog(rc, this, 0);
+        }
     }
+    
+    protected void onActivityResult(final int requestCode, final int resultCode, final Intent data) {
+        if (requestCode == AE_ACCT_REQUEST && resultCode == RESULT_OK) {		
+            String accountName = data.getStringExtra(AccountManager.KEY_ACCOUNT_NAME);
+            Toast.makeText(getApplicationContext(), "USE ACCOUNT [" + accountName + "]", Toast.LENGTH_SHORT).show();
+            new getAndUseAuthTokenInAsyncTask().execute(accountName);
+        }
+    }
+    
+    private String getAndUseAuthTokenBlocking(String accountName) {
+        String token = null;
+        String scope = "oauth2:https://www.googleapis.com/auth/plus.me https://www.googleapis.com/auth/userinfo.profile";
+        try {
+           token = GoogleAuthUtil.getToken(this, accountName, scope);
+           mss1 = "token";
+        } catch (GooglePlayServicesAvailabilityException playEx) {
+        	mss1 = "1" + playEx.getMessage();
+            Dialog dialog = GooglePlayServicesUtil.getErrorDialog(
+                playEx.getConnectionStatusCode(),
+                this,
+                AE_ACCT_REQUEST);
+            // Use the dialog to present to the user.
+        } catch (UserRecoverableAuthException recoverableException) {
+            Intent recoveryIntent = recoverableException.getIntent();
+            // Use the intent in a custom dialog or just startActivityForResult.
+            mss1 = "2" + recoverableException.getMessage();
+            startActivityForResult(recoverableException.getIntent(), REQUEST_TOKEN);
+
+        } catch (GoogleAuthException authEx) {
+            // This is likely unrecoverable.
+            Log.e(TAG, "Unrecoverable authentication exception: " + authEx.getMessage(), authEx);
+            mss1 = "3" + authEx.toString();
+        } catch (IOException ioEx) {
+            Log.i(TAG, "transient error encountered: " + ioEx.getMessage());
+            mss1 = "4" + ioEx.getMessage();
+        }
+        return token;
+    }
+    
+    String mss1="";
+    String mss2="";
+    
+    //====================================================================
+    // The SendActionAsyncTask calls the service to send the action to the
+    // AeProxy.  Android throws an exception if the
+    // you try to create a network exception in the users thread.
+    //====================================================================
+    class getAndUseAuthTokenInAsyncTask extends AsyncTask<String, Void, String > {
+
+        @Override
+        protected String doInBackground(String ...params) {
+            try {
+            	 String accountName = params[0];
+            	 return getAndUseAuthTokenBlocking(accountName);
+            } catch (Exception e) {
+                 token = null;
+                 mss2 = e.getMessage();
+                 Log.e("doInBackGround", e.getMessage());
+            }
+            return null;
+        }
+        
+        protected void onPostExecute(String result) {
+			if(result == null) {
+				token = null;
+				Log.e("onPostExecute", "Token is null");
+		       	Toast.makeText(getApplicationContext(), "Token=[null]", Toast.LENGTH_SHORT).show();
+		       	Toast.makeText(getApplicationContext(), "mss1=["+mss1+"]", Toast.LENGTH_SHORT).show();
+		       	Toast.makeText(getApplicationContext(), "mss2=["+mss2+"]", Toast.LENGTH_SHORT).show();
+			}
+			else {
+				token = result;
+				Log.e("onPostExecute", "Token=[" + token + "]");
+		       	Toast.makeText(getApplicationContext(), "Token=[" + token + "]", Toast.LENGTH_SHORT).show();
+			}
+        }
+    }
+    
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
